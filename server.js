49d997f3b414b64bf1b5b12fbbff6f781102c56f
David <david@outlier.org>
2019-06-14 12:26:08 -0700

    feat: add base64 endpoint

--- server.js
+++ server.js
@@ -1,24 +1,30 @@
-const http = require('http')
+const http = require("http");
 
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
-  if (req.url === '/') return respondHello(req, res)
-  if (req.url === '/user-agent') return respondUserAgent(req, res)
+  if (req.url === "/") return respondHello(req, res);
+  if (req.url === "/user-agent") return respondUserAgent(req, res);
+  if (req.url.match(/^\/b64\//)) return respondBase64(req, res);
 
-  res.end()
-})
+  res.end();
+});
 
-function respondHello (req, res) {
-  res.end(JSON.stringify({ msg: 'hello' }))
+function respondHello(req, res) {
+  res.end(JSON.stringify({ msg: "hello" }));
 }
 
-function respondUserAgent (req, res) {
-  const ua = req.headers['user-agent']
-  res.end(JSON.stringify({ ua }))
+function respondUserAgent(req, res) {
+  const ua = req.headers["user-agent"];
+  res.end(JSON.stringify({ ua }));
 }
 
-server.listen(PORT)
-console.log(`Server listening on port ${PORT}`)
+function respondBase64(req, res) {
+  const phrase = req.url.replace(/^\/b64\//, "");
+  res.end(JSON.stringify({ b64: Buffer.from(phrase).toString("base64") }));
+}
 
-if (require.main !== module) module.exports = server
+server.listen(PORT);
+console.log(`Server listening on port ${PORT}`);
+
+if (require.main !== module) module.exports = server;
